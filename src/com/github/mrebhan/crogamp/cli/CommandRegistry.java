8cad84a317ecd51b8b1bbbf88943bbeca8d17fad
Marco Rebhan <marco@UltraFarfetchd>
2016-09-28 18:47:55 +0200

    CLI basics.

--- src/com/github/mrebhan/crogamp/cli/CommandRegistry.java
+++ src/com/github/mrebhan/crogamp/cli/CommandRegistry.java
@@ -1,17 +1,69 @@
 package com.github.mrebhan.crogamp.cli;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 public class CommandRegistry {
 
+	private Map<String, Function<String[], Integer>> commands;
+	private Map<String, String> descriptions;
+
 	public CommandRegistry() {
-		
+		commands = new HashMap<>();
+		descriptions = new HashMap<>();
+	}
+
+	public int execute(String[] command) {
+		if (command.length > 0) {
+			Function<String[], Integer> f = commands.get(command[0]);
+			if (f != null) {
+				return f.apply(giveMeDaStuff(command));
+			}
+		}
+		return -1;
+	}
+
+	public void registerListCommands(String name) {
+		registerCommand(name, this::listCmds);
 	}
 	
-	public void execute(String[] command) {
-		
+	public void registerCommand(String name, Function<String[], Integer> function) {
+		commands.put(name, function);
 	}
 	
-	public void registerCommand() {
-		
+	private void addDescriptionText(String name, String description) {
+		descriptions.put(name, description);
+	}
+
+	public void registerCommand(String name, String description, Function<String[], Integer> function) {
+		registerCommand(name, function);
+		addDescriptionText(name, description);
+	}
+
+	private int listCmds(String[] in) {
+		int i = 2;
+		for (String string : commands.keySet()) {
+			i = Math.max(i, string.length() + 1);
+		}
+		for (String string : commands.keySet()) {
+			String out = string;
+			while (out.length() < i) {
+				out += ' ';
+			}
+			out += "| ";
+			if (descriptions.containsKey(string)) {
+				out += descriptions.get(string);
+			}
+			System.out.println(out);
+		}
+		return 0;
 	}
 	
+	private static String[] giveMeDaStuff(String[] commandline) {
+		String[] arguments = new String[commandline.length - 1];
+		System.arraycopy(commandline, 1, arguments, 0, arguments.length);
+		return arguments;
+	}
+
 }
--- src/com/github/mrebhan/crogamp/cli/CrogampCLI.java
+++ src/com/github/mrebhan/crogamp/cli/CrogampCLI.java
@@ -9,21 +9,48 @@ import com.github.mrebhan.crogamp.ICrogampInterface;
 
 public class CrogampCLI implements ICrogampInterface {
 
+	private CommandRegistry reg;
+
+	public CrogampCLI() {
+		reg = new CommandRegistry();
+		reg.registerListCommands("help");
+		registerCommands();
+	}
+
 	@Override
 	public int start(String[] args) {
-		System.out.printf("Welcome to Crogamp %s!%nType `LICENSE' for license info.%n", Crogamp.VERSION);
+		System.out.printf("Crogamp %s  Copyright (C) 2016  Marco Rebhan%nThis program comes "
+				+ "with ABSOLUTELY NO WARRANTY; for details type `license'.%n", Crogamp.VERSION);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			while (true) {
 				System.out.print("> ");
 				String[] stuffs = CommandUtil.split(reader.readLine());
 				if (stuffs.length > 0) {
-					
+					reg.execute(stuffs);
 				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return 0;
+	}
+
+	private void registerCommands() {
+		reg.registerCommand("license", "Displays licensing info.", this::cmdLicense);
+	}
+
+	private int cmdLicense(String[] args) {
+		System.out.printf("This program is free software; you can redistribute it and/or "
+				+ "modify%nit under the terms of the GNU General Public License"
+				+ " as published by%nthe Free Software Foundation; either versi"
+				+ "on 3 of the License, or%n(at your option) any later version."
+				+ "%n%nThis program is distributed in the hope that it will be "
+				+ "useful,%nbut WITHOUT ANY WARRANTY; without even the implied "
+				+ "warranty of%nMERCHANTABILITY or FITNESS FOR A PARTICULAR PUR"
+				+ "POSE.  See the%nGNU General Public License for more details."
+				+ "%n%nYou should have received a copy of the GNU General Publi"
+				+ "c License%nalong with this program. If not, see http://www.gnu.org/licenses/.%n");
 		return 0;
 	}
 
